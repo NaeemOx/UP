99bb66099f2b4bc4ae5280ae0887116df76a1798
NaeemOx <naeemabozed2@gmail.com>
2025-10-24 23:58:50 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,17 +1,1 @@
-// Manufacturing System - Data
-// Sample orders data
-
-const ordersData = [
-    {
-        id: 'PO-001',
-        productName: 'Custom Cabinet',
-        customer: 'ABC Furniture Co.',
-        quantity: 5,
-        startDate: '2025-10-01',
-        dueDate: '2025-11-15',
-        stages: [],
-        bom: []
-    }
-];
-
-// Add more data here
+console.log('Sample data loaded');
